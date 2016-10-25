5e9fafc6f22787a6b526e21bcee5956687fb6d4f
EDiLD <eduardszoecs@gmail.com>
2016-10-25 22:49:29 +0200

    added lognormal vs gamma vs zaga

---
+++ lnorm_vs_gamma_vs_zaga/server.R
@@ -0,0 +1,31 @@
+
+
+library(shiny)
+library(gamlss)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    # input to objects
+    mu <- input$mu
+    sigma <- input$sigma
+    nu <- input$nu
+    
+    # transform parameters
+    meanlog    = log(mu^2 / sqrt(sigma^2 + mu^2))
+    sdlog = sqrt(log((sigma^2 / mu^2) + 1))
+    
+    shape = 1 / sigma^2
+    scale = mu / shape
+    
+    # plot density
+    curve(dlnorm(x, meanlog = meanlog, sdlog = sdlog) , 0, 4)
+    curve(dgamma(x, scale = scale, shape = shape), 0, 4, 
+          col = 'red', add = TRUE)
+    curve(dZAGA(x, mu = mu, sigma = sigma, nu = nu), 0, 4,
+          col = 'blue', add = TRUE)
+    
+  })
+  
+})
---
+++ lnorm_vs_gamma_vs_zaga/ui.R
@@ -0,0 +1,37 @@
+
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Lognormal vs Gamma vs ZAGA"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("mu",
+                   "mu:",
+                   min = 0.5,
+                   max = 2,
+                   value = 2,
+                   step = 0.1),
+       sliderInput("sigma",
+                   "sigma:",
+                   min = 0,
+                   max = 1,
+                   value = 0.5,
+                   step = 0.1),
+       sliderInput("nu",
+                   "nu:",
+                   min = 0,
+                   max = 1,
+                   value = 0,
+                   step = 0.1)
+    ),
+    
+    # Show a plot of the density functions
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
